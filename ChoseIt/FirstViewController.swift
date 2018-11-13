c5175c97e81b4db7ae98722e1534fb56956e76fc
Patrick Korianski <pkorianski.engr@gmail.com>
2018-11-12 22:34:04 -0500

    Initial Commit

---
+++ ChoseIt/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ChoseIt
+//
+//  Created by Patrick Korianski on 11/12/18.
+//  Copyright © 2018 Patrick Korianski. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ChoseIt/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ChoseIt
+//
+//  Created by Patrick Korianski on 11/12/18.
+//  Copyright © 2018 Patrick Korianski. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
